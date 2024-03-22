0c202c338871249a6449105d338be847f86a0629
Tamila Ahmedivna Syniak <tamilaahmedivnasyniak@Tamilas-MacBook-Pro.local>
2024-03-22 12:05:24 +0200

    Updated path.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import './assets/index.css'
+import './index.css'
 import store from './store/store'
-import App from './components/App'
+import App from './components/app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode >
